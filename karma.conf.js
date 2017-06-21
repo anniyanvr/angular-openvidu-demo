4949f49332c05e3c51b80b1b18a33ee4d96ad514
Alex <alxmorais8@msn.com>
2017-06-21 21:08:30 +0200

    fix singlerun for travis builds

--- karma.conf.js
+++ karma.conf.js
@@ -1,10 +1,32 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/0.13/config/configuration-file.html
+let isCI = !!process.env.CI;
+let browsers = ['Chrome'];
+let autoWatch =  !isCI;
+let singleRun = isCI;
 
 module.exports = function (config) {
 	config.set({
+		// base path that will be used to resolve all patterns (eg. files, exclude)
 		basePath: '',
+		
+		// frameworks to use
+		// available frameworks: https://npmjs.org/browse/keyword/karma-adapter
 		frameworks: ['jasmine', '@angular/cli'],
+		
+		// list of files / patterns to load in the browser
+		files: [
+			{pattern: './src/test.ts', watched: false}
+		],
+		
+		// list of files to exclude
+		exclude: [
+		],
+		
+		// preprocess matching files before serving them to the browser
+		// available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+		preprocessors: {
+			'./src/test.ts': ['@angular/cli']
+		},
+		
 		plugins: [
 			require('karma-jasmine'),
 			require('karma-chrome-launcher'),
@@ -12,33 +34,50 @@ module.exports = function (config) {
 			require('karma-coverage-istanbul-reporter'),
 			require('@angular/cli/plugins/karma')
 		],
+		
 		client: {
 			clearContext: false // leave Jasmine Spec Runner output visible in browser
 		},
-		files: [
-			{pattern: './src/test.ts', watched: false}
-		],
-		preprocessors: {
-			'./src/test.ts': ['@angular/cli']
-		},
+		
 		mime: {
 			'text/x-typescript': ['ts', 'tsx']
 		},
+		
 		coverageIstanbulReporter: {
 			reports: ['html', 'lcovonly'],
 			fixWebpackSourcePaths: true
 		},
+		
 		angularCli: {
 			environment: 'dev'
 		},
+		
+		// test results reporter to use
+		// possible values: 'dots', 'progress'
+		// available reporters: https://npmjs.org/browse/keyword/karma-reporter
 		reporters: config.angularCli && config.angularCli.codeCoverage
 				? ['progress', 'coverage-istanbul']
 				: ['progress', 'kjhtml'],
+				
+		// web server port
 		port: 9876,
+		
+		// enable / disable colors in the output (reporters and logs)
 		colors: true,
+		
+		// level of logging
+		// possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
 		logLevel: config.LOG_INFO,
-		autoWatch: true,
-		browsers: ['Chrome'],
-		singleRun: false
+		
+		// enable / disable watching file and executing tests whenever any file changes
+		autoWatch: autoWatch,
+		
+		// start these browsers
+		// available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+		browsers: browsers,
+		
+		// Continuous Integration mode
+		// if true, Karma captures browsers, runs the tests and exits
+		singleRun: singleRun
 	});
 };
