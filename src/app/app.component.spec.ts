745f261ae9b65be445c0bfcbfe212cde230ff4d9
Alex <alxmorais8@msn.com>
2017-06-21 20:16:30 +0200

    fix tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,13 @@
 import { TestBed, async } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
+import { AppModule } from './app.module';
 
 describe('AppComponent', () => {
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
-			declarations: [
-				AppComponent
+			imports: [
+				AppModule
 			],
 		}).compileComponents();
 	}));
@@ -17,16 +18,12 @@ describe('AppComponent', () => {
 		expect(app).toBeTruthy();
 	}));
 
-	it(`should have as title 'app works!'`, async(() => {
+	it(`should have as title 'Angular OpenVidu Demo'`, async(() => {
 		const fixture = TestBed.createComponent(AppComponent);
 		const app = fixture.debugElement.componentInstance;
-		expect(app.title).toEqual('app works!');
-	}));
-
-	it('should render title in a h1 tag', async(() => {
-		const fixture = TestBed.createComponent(AppComponent);
-		fixture.detectChanges();
 		const compiled = fixture.debugElement.nativeElement;
-		expect(compiled.querySelector('h1').textContent).toContain('app works!');
+		console.log(app);
+		console.log(compiled);
+		expect(compiled.querySelector('md-toolbar').textContent.trim()).toEqual('Angular OpenVidu Demo');
 	}));
 });
